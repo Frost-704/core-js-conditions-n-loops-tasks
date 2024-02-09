2b8c5f5a638cf612e6b3d37124f5bf61f0228d28
Frost-704 <boolean.pg.me@gmail.com>
2024-02-09 01:51:03 +0100

    feat: add solution for canQueenCaptureKing

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -66,8 +66,25 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (queen.x === king.x || queen.y === king.y) {
+    return true;
+  }
+  for (let i = 0; i <= 8; i += 1) {
+    if (queen.x + i === king.x && queen.y + i === king.y) {
+      return true;
+    }
+    if (queen.x + i === king.x && queen.y - i === king.y) {
+      return true;
+    }
+    if (queen.x - i === king.x && queen.y + i === king.y) {
+      return true;
+    }
+    if (queen.x - i === king.x && queen.y - i === king.y) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
